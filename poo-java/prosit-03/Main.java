7a16dc15b815acacb3033c6a85d37cd72380710e
djebby <djebby.firas@gmail.com>
2022-06-30 06:30:38 +0100

    poo-java prosit03 added

---
+++ poo-java/prosit-03/Main.java
@@ -0,0 +1,52 @@
+package tn.tuniprob.gestionmagasin;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ProduitAlimentaire produitAlimentaireLait = new ProduitAlimentaire(1021, "Lait", "Delice", 0.7f);
+        ProduitAlimentaire produitAlimentaireYaourt = new ProduitAlimentaire(2410, "Yaourt", "Vitalait", 0.5f);
+        ProduitAlimentaire produitAlimentaireYaourt02 = new ProduitAlimentaire(2411, "Yaourt", "Natilait", 0.55f);
+        ProduitAlimentaire produitAlimentaireSicam = new ProduitAlimentaire(3250, "Tomate", "Sicam", 1.200f);
+
+        produitAlimentaireLait.setDateDexpiration(LocalDate.of(2023, 06, 25));
+        produitAlimentaireYaourt.setDateDexpiration(LocalDate.of(2023, 06, 25));
+        produitAlimentaireYaourt02.setDateDexpiration(LocalDate.of(2023, 06, 25));
+        produitAlimentaireSicam.setDateDexpiration(LocalDate.of(2023, 06, 25));
+
+        // test 1) a.
+        System.out.println( produitAlimentaireYaourt.comparer(produitAlimentaireYaourt02) );
+        // test 2) b.
+        System.out.println( ProduitAlimentaire.comparer(produitAlimentaireLait, produitAlimentaireLait) );
+
+        Magasin magasin1 = new Magasin();
+        magasin1.setIdentifiant(101);
+        magasin1.setAdresse("Beja Rue Bizert");
+        magasin1.setCapaciteMagasin(50);
+        magasin1.ajouterNouveauProduit(produitAlimentaireLait);
+        magasin1.ajouterNouveauProduit(produitAlimentaireYaourt);
+        magasin1.ajouterNouveauProduit(produitAlimentaireYaourt02);
+
+        Magasin magasin2 = new Magasin();
+        magasin2.setIdentifiant(102);
+        magasin2.setAdresse("BEJA NORD, Zone industrielle RTE TABARKA");
+        magasin2.setCapaciteMagasin(50);
+        magasin2.ajouterNouveauProduit(produitAlimentaireSicam);
+
+        // test 2éme question
+        System.out.println(magasin1.estIlExist(1021));
+        System.out.println(magasin2.estIlExist(0523));
+
+        // test 3éme question
+        magasin2.ajouterNouveauProduit(produitAlimentaireSicam);
+
+        // test 4éme question
+        magasin1.afficher();
+        magasin1.supprimerProduit(1021);
+        magasin1.afficher();
+
+        // test 5éme question
+        System.out.println( Magasin.superieurMagasin(magasin1, magasin2) );
+    }
+}
---
+++ poo-java/prosit-03/ProduitAlimentaire.java
@@ -0,0 +1,104 @@
+package tn.tuniprob.gestionmagasin;
+
+import java.time.LocalDate;
+
+public class ProduitAlimentaire {
+    private int identifiant;
+    private String libelle;
+    private String marque;
+    private float prix;
+
+    private LocalDate dateDexpiration;
+
+
+    public int getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        if(prix > 0)
+            this.prix = prix;
+    }
+
+    public void setDateDexpiration(LocalDate dateDexpiration) {
+        this.dateDexpiration = dateDexpiration;
+    }
+
+    public LocalDate getDateDexpiration() {
+        return dateDexpiration;
+    }
+
+    public ProduitAlimentaire() {
+    }
+
+    public ProduitAlimentaire(int identifiant, String libelle, String marque) {
+        this.identifiant = identifiant;
+        this.libelle = libelle;
+        this.marque = marque;
+    }
+
+    public ProduitAlimentaire(int identifiant, String libelle, String marque, float prix) {
+        this.identifiant = identifiant;
+        this.libelle = libelle;
+        this.marque = marque;
+        this.prix = prix;
+    }
+    public void afficher(){
+        System.out.println("identifiant: " + this.identifiant);
+        System.out.println("libelle: " + this.libelle);
+        System.out.println("marque: " + this.marque);
+        System.out.println("prix: " + this.prix + " TND");
+    }
+
+    @Override
+    public String toString() {
+        return "identifiant=" + identifiant +
+                "\nlibelle=" + libelle +
+                "\nmarque=" + marque +
+                "\nprix=" + prix + " TND";
+    }
+
+    // 1) a. méthode « comparer » prend un seul produit en paramètre return boolean
+    public boolean comparer(ProduitAlimentaire produitAlimentaire) {
+        if( produitAlimentaire.identifiant == this.identifiant &&
+                produitAlimentaire.libelle == this.libelle &&
+                produitAlimentaire.prix == this.prix  ) {
+            return true;
+        }
+        return false;
+    }
+
+    // 1) b. méthode « comparer » prend deux produits en paramètre
+    public static boolean comparer(ProduitAlimentaire produitAlimentaire01, ProduitAlimentaire produitAlimentaire02) {
+        if( produitAlimentaire01.identifiant == produitAlimentaire02.identifiant &&
+                produitAlimentaire01.libelle == produitAlimentaire02.libelle &&
+                produitAlimentaire01.prix == produitAlimentaire02.prix  ) {
+            return true;
+        }
+        return false;
+    }
+}
